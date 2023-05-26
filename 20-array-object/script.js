a80620f208dc35215e8e23a16864afd7c42cb5bc
Anne <steamengine.a@gmail.com>
2023-05-26 00:05:18 -0400

    Update 20-array-object

--- 20-array-object/script.js
+++ 20-array-object/script.js
@@ -1,7 +1,5 @@
 var car = ["Tesla", 80000, "white"];
 var car2 = { name: "Tesla", price: [70000, 80000, 120000] }; // key : value
-console.log(car.sort()); //array 순서대로 자료 저장; 정렬 가능 number-uppercase-lowercase
-console.log(car2["name"]);
-console.log(car2.price); //object 이름 붙여서 자료 저장 - 순서 개념 x
-console.log(car2.price[0]);
+
 document.querySelector(".name").innerHTML = car2.name;
+document.querySelector(".price").innerHTML = car2.price[1];
