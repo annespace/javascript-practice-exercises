79c45be48a68adac4303a98d0b24a1717a2ec311
Anne <steamengine.a@gmail.com>
2023-06-04 14:21:30 -0400

    Update script-syntax.js

--- 24-array-sort-map-filter/script-syntax.js
+++ 24-array-sort-map-filter/script-syntax.js
@@ -11,3 +11,23 @@ anne.sort(function (a, b) {
   return a - b;
 });
 console.log(anne);
+
+// array 자료 원하는 것만 필터, .filter() ex) 가격대 설정
+var anne3 = [7, 3, 5, 2, 40];
+// 신문법이기 때문에 .filter() 결과는 변수에 저장해서 써야함
+var anne3final = anne3.filter(function (a) {
+  //a: parameter, array 안에 있던 하나하나의 자료들
+  return a < 4; //어떤 자료를 남길건지
+});
+console.log(anne3final);
+
+/* .sort()는 원본 변형 O
+.filter()는 원본 변형 X --> 그래서 변수에 담아서 써야함
+ */
+
+// array 자료 전부 변형하려면, .map() ex) WON - > CAD/USD 변경
+var anne4 = [7, 3, 5, 2, 40];
+var anne4final = anne4.map(function (a) {
+  return a * 4;
+});
+console.log(anne4final);
